5d4454fc9a49e4bc778a2c95268b42f3fb9ff047
chfw <wangc_2011@hotmail.com>
2019-04-11 23:09:40 +0100

    :books: update static information

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ all: test
 
 test:
 	bash test.sh
+
+
+lint:
+	make lint
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
+# Template by pypi-mobans
 import codecs
 import locale
-
-# Template by pypi-mobans
 import os
 import platform
 import sys
 from shutil import rmtree
 
 from setuptools import Command, find_packages, setup
+
 
 # Work around mbcs bug in distutils.
 # http://bugs.python.org/issue10945
@@ -29,34 +29,45 @@ AUTHOR = "pyecharts dev team"
 VERSION = "0.0.1"
 EMAIL = "info@pyecharts.com"
 LICENSE = "MIT"
-DESCRIPTION = "Render echarts using selenium"
+DESCRIPTION = (
+    "Render echarts using selenium"
+)
 URL = "https://github.com/pyecharts/snapshot-selenium"
 DOWNLOAD_URL = "%s/archive/0.0.1.tar.gz" % URL
-FILES = ["README.md"]
-
+FILES = ["README.rst", "CHANGELOG.rst"]
+KEYWORDS = [
+    "python",
+]
 
 CLASSIFIERS = [
     "Topic :: Software Development :: Libraries",
     "Programming Language :: Python",
     "Intended Audience :: Developers",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
 ]
 
-INSTALL_REQUIRES = ["selenium"]
+INSTALL_REQUIRES = [
+    "selenium",
+]
 SETUP_COMMANDS = {}
 
 
 PACKAGES = find_packages(exclude=["ez_setup", "examples", "tests"])
-EXTRAS_REQUIRE = {}
+EXTRAS_REQUIRE = {
+}
 # You do not need to read beyond this line
 PUBLISH_COMMAND = "{0} setup.py sdist bdist_wheel upload -r pypi".format(sys.executable)
-GS_COMMAND = "gs snapshot-selenium v0.0.1 " + "Find 0.0.1 in changelog for more details"
-NO_GS_MESSAGE = (
-    "Automatic github release is disabled. " + "Please install gease to enable it."
-)
-UPLOAD_FAILED_MSG = 'Upload failed. please run "%s" yourself.' % PUBLISH_COMMAND
+GS_COMMAND = ("gs snapshot-selenium v0.0.1 " +
+              "Find 0.0.1 in changelog for more details")
+NO_GS_MESSAGE = ("Automatic github release is disabled. " +
+                 "Please install gease to enable it.")
+UPLOAD_FAILED_MSG = (
+    'Upload failed. please run "%s" yourself.' % PUBLISH_COMMAND)
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -99,7 +110,9 @@ class PublishCommand(Command):
         sys.exit()
 
 
-SETUP_COMMANDS.update({"publish": PublishCommand})
+SETUP_COMMANDS.update({
+    "publish": PublishCommand
+})
 
 
 def has_gease():
@@ -110,7 +123,6 @@ def has_gease():
     """
     try:
         import gease  # noqa
-
         return True
     except ImportError:
         return False
@@ -170,6 +182,7 @@ if __name__ == "__main__":
         download_url=DOWNLOAD_URL,
         long_description=read_files(*FILES),
         license=LICENSE,
+        keywords=KEYWORDS,
         extras_require=EXTRAS_REQUIRE,
         tests_require=["nose"],
         install_requires=INSTALL_REQUIRES,
@@ -177,5 +190,5 @@ if __name__ == "__main__":
         include_package_data=True,
         zip_safe=False,
         classifiers=CLASSIFIERS,
-        cmdclass=SETUP_COMMANDS,
+        cmdclass=SETUP_COMMANDS
     )
