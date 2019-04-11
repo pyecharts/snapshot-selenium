645c5d2be35dd89bd47e23b04cd1572f439a82af
chfw <wangc_2011@hotmail.com>
2019-04-11 23:28:42 +0100

    :egg: :ferris_wheel: release 0.0.2

--- setup.py
+++ setup.py
@@ -26,14 +26,14 @@ except (ValueError, UnicodeError, locale.Error):
 
 NAME = "snapshot-selenium"
 AUTHOR = "pyecharts dev team"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 EMAIL = "info@pyecharts.com"
 LICENSE = "MIT"
 DESCRIPTION = (
     "Render echarts using selenium"
 )
 URL = "https://github.com/pyecharts/snapshot-selenium"
-DOWNLOAD_URL = "%s/archive/0.0.1.tar.gz" % URL
+DOWNLOAD_URL = "%s/archive/0.0.2.tar.gz" % URL
 FILES = ["README.rst", "CHANGELOG.rst"]
 KEYWORDS = [
     "python",
@@ -64,8 +64,8 @@ EXTRAS_REQUIRE = {
 }
 # You do not need to read beyond this line
 PUBLISH_COMMAND = "{0} setup.py sdist bdist_wheel upload -r pypi".format(sys.executable)
-GS_COMMAND = ("gs snapshot-selenium v0.0.1 " +
-              "Find 0.0.1 in changelog for more details")
+GS_COMMAND = ("gs snapshot-selenium v0.0.2 " +
+              "Find 0.0.2 in changelog for more details")
 NO_GS_MESSAGE = ("Automatic github release is disabled. " +
                  "Please install gease to enable it.")
 UPLOAD_FAILED_MSG = (
