a8453c55ebefe33f1b15bf4bf5f8aa29b366d11f
chfw <wangc_2011@hotmail.com>
2019-04-12 08:31:54 +0100

    :sparkles: 3.7 included

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
 ]
 
 INSTALL_REQUIRES = ["selenium"]
